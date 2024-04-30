2d5ccc4111b516c264c44caca25651cb4df2054d
ashwinfelix <35266735+ashwinfelix@users.noreply.github.com>
2024-04-30 17:19:05 +0530

    some changes like adding a bg image

--- ashwin123/src/App.js
+++ ashwin123/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Header from './Header';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 function App() {
   return (
